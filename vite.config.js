fe6e90e08855ba7ab0248048881232abd947d184
MyResellApp <myresellapp@gmail.com>
2025-06-19 18:50:43 +0200

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,16 @@ const isDev = process.env.NODE_ENV !== 'production';
 let inlineEditPlugin, editModeDevPlugin;
 
 if (isDev) {
-	inlineEditPlugin = (await import('./plugins/visual-editor/vite-plugin-react-inline-editor.js')).default;
-	editModeDevPlugin = (await import('./plugins/visual-editor/vite-plugin-edit-mode.js')).default;
+	try {
+		inlineEditPlugin = (await import('./plugins/visual-editor/vite-plugin-react-inline-editor.js')).default;
+		editModeDevPlugin = (await import('./plugins/visual-editor/vite-plugin-edit-mode.js')).default;
+	} catch (e) {
+		console.warn('Editor plugins no disponibles en producción o entorno sin archivos.');
+		inlineEditPlugin = () => ({ name: 'noop-inline-edit' });
+		editModeDevPlugin = () => ({ name: 'noop-edit-mode' });
+	}
 }
+
 
 const configHorizonsViteErrorHandler = `
 const observer = new MutationObserver((mutations) => {
